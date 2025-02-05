9d32f0179ba9f38d24fa11b851f8468d438d7e68
arthrod <89408329+arthrod@users.noreply.github.com>
2025-02-05 04:44:34 -0500

    Discard changes to src/components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,18 @@
 import 'virtual:svg-icons-register'
 
 import type { UmoEditorOptions } from '@/types'
-import { useStore } from '@/composables/store'
+
 import UmoEditor from './index.vue'
 import UmoMenuButton from './menus/button.vue'
 import UmoDialog from './modal.vue'
 import UmoTooltip from './tooltip.vue'
 
-const store = useStore()
-
 const useUmoEditor = {
   install: (app: any, options: UmoEditorOptions) => {
-    // Initialize store with options
-    store.setOptions(options)
-    // Register component
+    // 组件配置
+    const { setOptions } = useStore()
+    setOptions(options)
+    // 使用组件
     app.component(UmoEditor.name ?? 'UmoEditor instance', UmoEditor)
   },
 }
