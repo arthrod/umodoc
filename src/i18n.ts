39a54c747e36c3e31f306eb09002317f702bff15
arthrod <89408329+arthrod@users.noreply.github.com>
2025-02-05 04:50:40 -0500

    Discard changes to src/i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,25 +1,20 @@
 import { isRecord } from '@tool-belt/type-predicates'
 import { merge } from 'ts-deepmerge'
 import { createI18n } from 'vue-i18n'
-import { useStore } from '@/composables/store'
-import { debugReadability } from '@/utils/logger'
 
 import type { SupportedLocale } from '@/types'
 
 import en_US from './locales/en-US.json'
-import zh_CN from './locales/zh-CN.json'
 import ru_RU from './locales/ru-RU.json'
+import zh_CN from './locales/zh-CN.json'
 
-const store = useStore()
-const { options } = store
+const { options } = useStore()
 
 const getLocale = (lang: SupportedLocale) => {
   const translations = options.value.translations?.[lang]
   if (isRecord(translations)) {
-    debugReadability('i18n', 'customTranslations', { lang })
     return translations
   }
-  debugReadability('i18n', 'defaultTranslations', { lang })
   return {}
 }
 
